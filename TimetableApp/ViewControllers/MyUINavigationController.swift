42237d4739b7a48bf64acbbe7be599fcc96fb61d
0429oonishi <oonishireon110@gmail.com>
2021-04-21 16:06:26 +0900

    navigationBarとtabBarの背景色を変更

---
+++ TimetableApp/ViewControllers/MyUINavigationController.swift
@@ -0,0 +1,19 @@
+//
+//  MyUINavigationController.swift
+//  TimetableApp
+//
+//  Created by 大西玲音 on 2021/04/21.
+//
+
+import UIKit
+
+final class MyUINavigationController: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        UINavigationBar.appearance().barTintColor = #colorLiteral(red: 0.9333333333, green: 0.9333333333, blue: 0.9333333333, alpha: 1)
+        
+    }
+
+}
---
+++ TimetableApp/ViewControllers/MyUITabBarController.swift
@@ -0,0 +1,19 @@
+//
+//  MyUITabBarController.swift
+//  TimetableApp
+//
+//  Created by 大西玲音 on 2021/04/21.
+//
+
+import UIKit
+
+final class MyUITabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        UITabBar.appearance().barTintColor = #colorLiteral(red: 0.9333333333, green: 0.9333333333, blue: 0.9333333333, alpha: 1)
+         
+    }
+
+}
