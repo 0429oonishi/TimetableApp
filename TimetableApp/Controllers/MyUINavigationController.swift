5bd14ebbd83e9cb31fdea42838bcf160430735bd
0429oonishi <oonishireon110@gmail.com>
2021-05-12 06:11:30 +0900

    MyUINavigationControllerを整理

--- TimetableApp/Controllers/MyUINavigationController.swift
+++ TimetableApp/Controllers/MyUINavigationController.swift
@@ -30,50 +30,49 @@ final class MyUINavigationController: UINavigationController {
 private extension MyUINavigationController {
     
     func setupTitleButton() {
-        let titleButtonWidth: CGFloat = 70
-        let titleButtonHeight: CGFloat = 30
-        let titleButton = NeumorphismView(
-            frame: CGRect(x: (navigationBar.frame.size.width - titleButtonWidth) / 2,
-                          y: (navigationBar.frame.size.height - titleButtonHeight) / 2,
-                          width: titleButtonWidth,
-                          height: titleButtonHeight
+        let buttonWidth: CGFloat = 70
+        let buttonHeight: CGFloat = 30
+        let button = NeumorphismView(
+            frame: CGRect(x: (navigationBar.frame.size.width - buttonWidth) / 2,
+                          y: (navigationBar.frame.size.height - buttonHeight) / 2,
+                          width: buttonWidth,
+                          height: buttonHeight
             )
         )
-        titleButton.type = .pushButton
-        titleButton.cornerRadius = titleButton.frame.size.height / 2
-        titleButton.addTarget(self, action: #selector(titleButtonDidTapped), for: .touchUpInside)
-        navigationBar.addSubview(titleButton)
+        button.type = .pushButton
+        button.cornerRadius = button.frame.size.height / 2
+        button.addTarget(self, action: #selector(titleButtonDidTapped), for: .touchUpInside)
+        navigationBar.addSubview(button)
         
         let label = UILabel()
         label.text = "タイトル"
         label.textColor = .black
         label.textAlignment = .center
         label.font = .boldSystemFont(ofSize: 14)
-        titleButton.addSubview(label)
-        label.anchor(centerY: titleButton.centerYAnchor, centerX: titleButton.centerXAnchor)
+        button.addSubview(label)
+        label.anchor(centerY: button.centerYAnchor, centerX: button.centerXAnchor)
     }
     
     func setupSettingButton() {
-        let settingButtonWidth: CGFloat = 32
-        let settingButtonHeight: CGFloat = 32
-        let settingButton = NeumorphismView(
+        let buttonWidth: CGFloat = 32
+        let buttonHeight: CGFloat = 32
+        let button = NeumorphismView(
             frame: CGRect(x: 15,
-                          y: (navigationBar.frame.size.height - settingButtonHeight) / 2,
-                          width: settingButtonWidth,
-                          height: settingButtonHeight
+                          y: (navigationBar.frame.size.height - buttonHeight) / 2,
+                          width: buttonWidth,
+                          height: buttonHeight
             )
         )
-        settingButton.type = .pushButton
-        settingButton.cornerRadius = settingButton.frame.size.height / 2
-        settingButton.addTarget(self, action: #selector(settingButtonDidTapped), for: .touchUpInside)
-        navigationBar.addSubview(settingButton)
+        button.type = .pushButton
+        button.cornerRadius = button.frame.size.height / 2
+        button.addTarget(self, action: #selector(settingButtonDidTapped), for: .touchUpInside)
+        navigationBar.addSubview(button)
         
         let imageView = UIImageView()
         imageView.image = UIImage(systemName: "gearshape")
         imageView.tintColor = .black
-        settingButton.addSubview(imageView)
-        imageView.anchor(centerY: settingButton.centerYAnchor, centerX: settingButton.centerXAnchor)
-        
+        button.addSubview(imageView)
+        imageView.anchor(centerY: button.centerYAnchor, centerX: button.centerXAnchor)
     }
     
 }
