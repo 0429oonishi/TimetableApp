8126c999b44c705e8815e4029ddd6cf0f71e74ac
akio0911 <akio0911@gmail.com>
2021-05-12 19:10:13 +0900

    Add manageableWeeks

--- TimetableApp/Models/Convert.swift
+++ TimetableApp/Models/Convert.swift
@@ -15,7 +15,7 @@ struct Convert {
     var number: Int {
         guard let week = week else { return -1 }
         guard let period = period else { return -1 }
-        return week.number + Week.data.count * period.number
+        return week.number + Week.manageableWeeks.count * period.number
     }
     
 }
