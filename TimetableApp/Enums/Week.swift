00e4be0c5ae72327e5c7507ae9294bd187911e3c
akio0911 <akio0911@gmail.com>
2021-05-12 19:14:02 +0900

    numberをprivate extensionで隔離

--- TimetableApp/Enums/Week.swift
+++ TimetableApp/Enums/Week.swift
@@ -41,17 +41,4 @@ extension Week {
             case .sunday: return "日"
         }
     }
-    
-    var number: Int {
-        switch self {
-            case .monday: return 0
-            case .tuesday: return 1
-            case .wednesday: return 2
-            case .thursday: return 3
-            case .friday: return 4
-            case .saturday: return 5
-            case .sunday: return 6
-        }
-    }
-    
 }
--- TimetableApp/Models/Convert.swift
+++ TimetableApp/Models/Convert.swift
@@ -20,3 +20,16 @@ struct Convert {
     
 }
 
+private extension Week {
+    var number: Int {
+        switch self {
+            case .monday: return 0
+            case .tuesday: return 1
+            case .wednesday: return 2
+            case .thursday: return 3
+            case .friday: return 4
+            case .saturday: return 5
+            case .sunday: return 6
+        }
+    }
+}
