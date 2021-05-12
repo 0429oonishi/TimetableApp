030169155fe1f1453e907611465d8835f762c8c3
akio0911 <akio0911@gmail.com>
2021-05-12 18:41:41 +0900

    TimetableSettingViewController.instantiateを追加

--- TimetableApp/Controllers/TimetableSettingViewController.swift
+++ TimetableApp/Controllers/TimetableSettingViewController.swift
@@ -12,6 +12,13 @@ final class TimetableSettingViewController: UIViewController {
     @IBOutlet private weak var tableView: UITableView!
     
     private let userDefault = UserDefault<UserDefaultsKeys.TimetableSettingType>()
+
+    static func instantiate() -> TimetableSettingViewController {
+        let timetableSettingVC = UIStoryboard.timetableSetting.instantiateViewController(
+            identifier: TimetableSettingViewController.identifier
+        ) as! TimetableSettingViewController
+        return timetableSettingVC
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
